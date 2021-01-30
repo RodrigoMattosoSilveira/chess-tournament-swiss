cfde69b7d60fa51bf15e42e194296f022a1d78c3
Rodrigo Silveira <r3850365@gmail.com>
2021-01-30 02:44:36 -0800

    integrate-dto-dao  -  Integrate DAO DTO layers using new entity, user (no testing)

---
+++ src/model/user/README.md
@@ -0,0 +1,69 @@
+# users
+Model a site user (not yet refactored to model our chess site user)
+
+#curl
+## GET ALL USERS
+Use the following `curl` command to retrieve all users:
+````bash
+$ curl --location --request GET 'localhost:3000/user' --header 'Content-Type: application/json'
+````
+
+## POST
+Use the following `curl` command to create a user:
+````bash
+curl --location --request POST 'localhost:3000/user' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+"email": "rodrigo.silveira@coolprovider.com",
+"password": "sup3rS3cr3tPassw0rd!23"
+}'
+````
+
+Once executed, the service returns a user id 
+````json
+{
+    "id": "ksVnfnPVW"
+}
+````
+
+Use the terminal to create an environment variable to help us with the remaining curl examples
+
+````bash
+$ REST_API_EXAMPLE_ID="put_your_id_here"
+````
+
+## GET A USER
+Use the following `curl` command to retrieve a user:
+````bash
+$ curl --location --request GET "localhost:3000/user/$REST_API_EXAMPLE_ID" --header 'Content-Type: application/json'
+````
+
+## PUT
+Use the following `curl` command to update the whole record:
+````bash
+$ curl --location --request PUT "localhost:3000/user/$REST_API_EXAMPLE_ID" \
+--header 'Content-Type: application/json' \
+--data-raw '{
+"email": "marcos.henrique@toptal.com",
+"password": "sup3rS3cr3tPassw0rd!23",
+"firstName": "Marcos",
+"lastName": "Silva",
+"permissionLevel": 8
+}'
+````
+## PATCH
+Use the following `curl` command to update the email address:
+````bash
+curl --location --request PATCH "localhost:3000/users/$REST_API_EXAMPLE_ID" \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    "lastName": "Mattoso"
+}'
+````
+
+## DELETE
+Use the following `curl` delete a user:
+````bash
+curl --location --request DELETE "localhost:3000/user/$REST_API_EXAMPLE_ID" \
+--header 'Content-Type: application/json'
+````
---
+++ src/model/user/user.routes.config.ts
@@ -0,0 +1,55 @@
+/**
+ * Define the User requests API
+ *
+ * Here, we are importing the CommonRoutesConfig class and extending it to our new class, called UserRoutes. With the
+ * constructor, we send the app (the main express.Application object) and the name UserRoutes to CommonRoutesConfig’s
+ * constructor.
+ *
+ * This example is quite simple, but when scaling to create several route files, this will help us avoid duplicate code.
+ *
+ * Suppose we would want to add new features in this file, such as logging. We could add the necessary field to the
+ * CommonRoutesConfig class, and then all the routes that extend CommonRoutesConfig will have access to it.
+ */
+
+import {CommonRoutesConfig} from '../../common/common.routes.config';
+import UserController from './user.controller';
+import UserMiddleware from './user.middleware';
+import express from 'express';
+
+export class UserRoutes extends CommonRoutesConfig {
+	constructor(app: express.Application) {
+		super(app, 'UserRoutes');
+	}
+	
+	/**
+	 * This logic lets any REST API client call our user endpoint with a POST or a GET request. Similarly, it lets a
+	 * client call our /user/:userId endpoint with a GET, PUT, PATCH, or DELETE request.
+	 */
+	configureRoutes() {
+		this.app.route(`/user`)
+			.get(UserController.listUsers)
+			.post(
+				UserMiddleware.validateRequiredUserBodyFields,
+				UserMiddleware.validateSameEmailDoesntExist,
+				UserController.createUser);
+		
+		this.app.param(`userId`, UserMiddleware.extractUserId);
+		this.app.route(`/user/:userId`)
+			.all(UserMiddleware.validateUserExists)
+			.get(UserController.getUserById)
+			.delete(UserController.removeUser);
+		
+		this.app.put(`/user/:userId`,[
+			UserMiddleware.validateRequiredUserBodyFields,
+			UserMiddleware.validateSameEmailBelongToSameUser,
+			UserController.put
+		]);
+		
+		this.app.patch(`/user/:userId`, [
+			UserMiddleware.validatePatchEmail,
+			UserController.patch
+		]);
+		
+		return this.app;
+	}
+}
