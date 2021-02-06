a7aac7aad63b05288e1580004dff47e5eea0c9f9
Rodrigo Silveira <r3850365@gmail.com>
2021-02-06 07:19:37 -0800

    user-tournament-tests  -  Removed console.log statements

--- src/model/user/user.controller.ts
+++ src/model/user/user.controller.ts
@@ -40,7 +40,7 @@ class UserController {
 	}
 	
 	async patch(req: express.Request, res: express.Response) {
-		console.log('\n' + 'UserController/patch' + JSON.stringify(req.body) +'\n');
+		// console.log('\n' + 'UserController/patch' + JSON.stringify(req.body) +'\n');
 		if(req.body.password){
 			req.body.password = await argon2.hash(req.body.password);
 		}
--- src/model/user/user.middleware.ts
+++ src/model/user/user.middleware.ts
@@ -23,7 +23,7 @@ class UserMiddleware {
 	}
 	
 	async validateEmail(req: express.Request, res: express.Response, next: express.NextFunction) {
-		console.log('\n' + 'UserMiddleware/validateEmail/email: ' + req.body.email + '\n');
+		// console.log('\n' + 'UserMiddleware/validateEmail/email: ' + req.body.email + '\n');
 		// https://emailregex.com/
 		const regexp = new RegExp(/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/);
 		if (req.body.email && !regexp.test(req.body.email)) {
@@ -56,8 +56,7 @@ class UserMiddleware {
 		if (!req.body.state) {
 			next()
 		} else {
-			console.log('\n' + 'UserMiddleware/validateStateIfPresent/value: ' + req.body.state + '\n');
-			
+			// console.log('\n' + 'UserMiddleware/validateStateIfPresent/value: ' + req.body.state + '\n');
 			// it is present, hence must be valid
 			if (!isStateSupported(req.body.state)) {
 				res.status(404).send({error: `state ${req.body.state} is invalid`});
@@ -81,7 +80,7 @@ export default UserMiddleware.getInstance();
  */
 const isStateSupported = (value: string): boolean => {
 	let _value = value.toLowerCase();
-	console.log('\n' + 'UserMiddleware/isStateSupported/value: ' + _value + '\n');
-	console.log('\n' + 'UserMiddleware/isStateSupported/user_states: ' + JSON.stringify(user_states) + '\n');
+	// console.log('\n' + 'UserMiddleware/isStateSupported/value: ' + _value + '\n');
+	// console.log('\n' + 'UserMiddleware/isStateSupported/user_states: ' + JSON.stringify(user_states) + '\n');
 	return user_states.findIndex((aValidType: string) => aValidType === _value) !== -1;
 }
