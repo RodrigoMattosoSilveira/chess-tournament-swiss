6e9b41b2488f206f839b1973d4b3d518cae3dcd6
Rodrigo Silveira <r3850365@gmail.com>
2021-02-14 18:31:41 -0800

    game  -  All validations for GET /player, POST /player. GET /player/id, and PATCH /player/id are working!

--- src/model/player/player.test.ts
+++ src/model/player/player.test.ts
@@ -1,3 +1,5 @@
+import exp from "constants";
+
 const request = require('supertest');
 
 import app from './../../index';
@@ -22,7 +24,7 @@ describe('Player Entity', () => {
 	let playerEntity: any;
 	let playerPatchEntity: any;
 	
-	describe('Player Entity GET /player', () => {
+	describe('GET /player', () => {
 		it('GET /player', async done => {
 			response = await request(app)
 				.get(resource)
@@ -40,7 +42,7 @@ describe('Player Entity', () => {
 			done();
 		})
 	});
-	describe('Player Entity POST', () => {
+	describe('POST /player and GET /player/id', () => {
 		beforeAll(async done => {
 			// Create a user to help POST a player
 			userEntity = {
@@ -50,7 +52,7 @@ describe('Player Entity', () => {
 			userDao.addUser(userEntity)
 				.then((id: string) => {
 					userId = id;
-					console.log('\nCreated user to help POST a player: ' + userId + '\n');
+					// console.log('\nCreated user to help POST a player: ' + userId + '\n');
 					
 					// Create a tournament to help POST a player
 					tournamentEntity = {
@@ -62,8 +64,8 @@ describe('Player Entity', () => {
 					tournamentDao.add(tournamentEntity)
 						.then((id: string) => {
 							tournamentId = id;
-							console.log('\nCreated tournament to help POST a player: ' + tournamentId + '\n');
-							console.log('\nCreated user and tournament to help POST a player: ' + userId + " " + tournamentId + '\n');
+							// console.log('\nCreated tournament to help POST a player: ' + tournamentId + '\n');
+							// console.log('\nCreated user and tournament to help POST a player: ' + userId + " " + tournamentId + '\n');
 						})
 						.catch((err: any) => {
 							// console.log('\nError creating a tournament to help POST a player: ' + err + '\n');
@@ -77,8 +79,8 @@ describe('Player Entity', () => {
 				});
 			done();
 		});
-		it('valid player', async done => {
-			console.log('\nAbout to POST a player: ' + userId + " " + tournamentId + '\n');
+		it('POST /player', async done => {
+			// console.log('\nAbout to POST a player: ' + userId + " " + tournamentId + '\n');
 			playerEntity = {
 				user: userId,
 				tournament: tournamentId
@@ -90,7 +92,7 @@ describe('Player Entity', () => {
 				.expect('Content-Type', /json/)
 				.expect(201)
 				.then((response: any) => {
-					console.log('\nPOSTed a player: ' + response.body.id + '\n');
+					// console.log('\nPOSTed a player: ' + response.body.id + '\n');
 					expect(response.body.id).toBeTruthy()
 					playerId = response.body.id;
 				})
@@ -98,18 +100,18 @@ describe('Player Entity', () => {
 					// console.log('\nPOST /player - invalid: ' + err + '\n');
 					done(err)
 				});
-			console.log('\nPOSTed a player 1: ' + playerId + '\n');
+			// console.log('\nPOSTed a player 1: ' + playerId + '\n');
 			done();
 		});
-		it('and get the posted player', async done => {
-			console.log('\nAbout to GET a posted: ' + userId + " " + playerId + '\n');
+		it('GET /player/id', async done => {
+			// console.log('\nAbout to GET a posted: ' + userId + " " + playerId + '\n');
 			await request(app)
 				.get("/player/" + playerId)
 				.set('Accept', 'application/json')
 				.expect('Content-Type', /json/)
 				.expect(200)
 				.then((response: any) => {
-					console.log('\nGot POSTed player: ' + response.body.id + '\n');
+					// console.log('\nGot POSTed player: ' + response.body.id + '\n');
 					expect(response.body.id).toEqual(playerId);
 					expect(response.body.user).toEqual(userId);
 					expect(response.body.tournament).toEqual(tournamentId);
@@ -125,131 +127,89 @@ describe('Player Entity', () => {
 					// console.log('\nPOST /player - invalid: ' + err + '\n');
 					done(err)
 				});
-			console.log('\nGot POSTed player: ' + playerId + '\n');
+			// console.log('\nGot POSTed player: ' + playerId + '\n');
+			done();
+		});
+	});
+	describe('PATCH /player:id', () => {
+		beforeAll(() => {
+			// console.log("About to validate the patch capability: " + playerId + "\n")
+		})
+		it('all patchable attributes', async done => {
+			// console.log("About to validate the patch capability: " + playerId + "\n");
+			let patchEntity: any = {
+				"hadByeOrForfeit": true,
+				"byeNextRound": true,
+				"playedAgainst": ["a1x_V1h5Mq", "mgjR2IAh72"],
+				"playedColor": [-1, 1],
+				"results": [0, 1],
+				"state": "inactive"
+			}
+			let response = await utils.patchEntity(request(app), "/player" + '/' + playerId, patchEntity);
+			expect(response.body.hadByeOrForfeit).toEqual(patchEntity.hadByeOrForfeit);
+			expect(response.body.byeNextRound).toEqual(patchEntity.byeNextRound);
+			expect(response.body.playedAgainst).toEqual(patchEntity.playedAgainst);
+			expect(response.body.playedColor).toEqual(patchEntity.playedColor);
+			expect(response.body.results).toEqual(patchEntity.results);
+			expect(response.body.state).toEqual(patchEntity.state);
+			done();
+		});
+		it('the hadByeOrForfeit attribute', async done => {
+			let patchEntity: any = {
+				"hadByeOrForfeit": false,
+			}
+			let response = await utils.patchEntity(request(app), "/player" + '/' + playerId, patchEntity);
+			expect(response.body.hadByeOrForfeit).toEqual(patchEntity.hadByeOrForfeit);
+			done();
+		});
+		it('the byeNextRound attribute', async done => {
+			let patchEntity: any = {
+				"byeNextRound": false,
+			}
+			let response = await utils.patchEntity(request(app), "/player" + '/' + playerId, patchEntity);
+			expect(response.body.byeNextRound).toEqual(patchEntity.byeNextRound);
+			done();
+		});
+		it('the playedColor attribute', async done => {
+			let patchEntity: any = {
+				"playedColor": [],
+			}
+			let response = await utils.patchEntity(request(app), "/player" + '/' + playerId, patchEntity);
+			expect(response.body.playedColor).toEqual(patchEntity.playedColor);
+			done();
+		});
+		it('the playedAgainst attribute', async done => {
+			let patchEntity: any = {
+				"playedAgainst": [],
+			}
+			let response = await utils.patchEntity(request(app), "/player" + '/' + playerId, patchEntity);
+			expect(response.body.playedAgainst).toEqual(patchEntity.playedAgainst);
+			done();
+		});
+		it('the playedColor attribute', async done => {
+			let patchEntity: any = {
+				"playedColor": [],
+			}
+			let response = await utils.patchEntity(request(app), "/player" + '/' + playerId, patchEntity);
+			expect(response.body.playedColor).toEqual(patchEntity.playedColor);
+			done();
+		});
+		it('the results attribute', async done => {
+			let patchEntity: any = {
+				"results": [],
+			}
+			let response = await utils.patchEntity(request(app), "/player" + '/' + playerId, patchEntity);
+			expect(response.body.results).toEqual(patchEntity.results);
+			done();
+		});
+		it('the state attribute', async done => {
+			let patchEntity: any = {
+				"state": PLAYER_STATE.ACTIVE,
+			}
+			let response = await utils.patchEntity(request(app), "/player" + '/' + playerId, patchEntity);
+			expect(response.body.state).toEqual(patchEntity.state);
 			done();
 		});
 
 	});
-	// 	it('POST /player - valid', async done => {
-	// 		let playerDto: PlayerDto = {
-	// 			id: "none",
-	// 			user: userId,
-	// 			tournament: tournamentId
-	// 		}
-	// 		request(app)
-	// 			.post("/player")
-	// 			.send(playerDto)
-	// 			.set('Accept', 'application/json')
-	// 			.expect('Content-Type', /json/)
-	// 			.expect(201)
-	// 			.then((response: any) => {
-	// 				// console.log('\Player Entity/POST /player: ' + response.body.id + '\n');
-	// 				expect(response.body.id).toBeTruthy()
-	// 				playerId = response.body.id;
-	// 				done();
-	// 			})
-	// 			.catch((err: any) => {
-	// 				// console.log('\nPOST /player - valid: ' + err + '\n');
-	// 				done(err)
-	// 			});
-	// 		response = await request(app)
-	// 			.get("/player/" + playerId)
-	// 			.set('Accept', 'application/json')
-	// 			.expect('Content-Type', /json/)
-	// 			.expect(200)
-	// 			.then((response: any) => {
-	// 				console.log('\nPlayer Entity/POST GET /player/ ' + response.body.id + '\n');
-	// 				expect(response.body.id).toEqual(playerId);
-	// 				expect(response.body.user).toEqual(userId);
-	// 				expect(response.body.tournament).toEqual(tournamentId);
-	// 				expect(response.body.hadByeOrForfeit).toEqual(false);
-	// 				expect(response.body.byeNextRound).toEqual(false);
-	// 				expect(response.body.playedAgainst).toEqual([]);
-	// 				expect(response.body.playedColor).toEqual([]);
-	// 				expect(response.body.results).toEqual([]);
-	// 				expect(response.body.state).toEqual(TOURNAMENT_STATE.SCHEDULED);
-	// 				done();
-	// 			})
-	// 			.catch((err: any) => {
-	// 				// console.log('\nGET /player: ' + err + '\n');
-	// 				done(err)
-	// 			});
-	// 		done();
-	// 	});
-	// })
-	// describe('Player Entity PATCH /player:id', () => {
-	// 	// Create this entity and validate PATCH against it
-	// 	// creat a valid user - we are not testing tournaments
-	// 	userEntity = {
-	// 		email: "a.b@c.com",
-	// 		password: "easytobreak"
-	// 	}
-	//
-	// 	//	create a valid tournament - we are not testing tournaments
-	// 	tournamentEntity = {
-	// 		name: "Blanchard Open - 2021",
-	// 		maxPlayers: 32,
-	// 		rounds: 6,
-	// 		type: TOURNAMENT_TYPE.SWISS
-	// 	}
-	// 	let playerEntity: any = {
-	// 		user: userId,
-	// 		tournament: tournamentId
-	// 	}
-	// 	beforeAll(async done => {
-	// 		// create a user
-	// 		userDao.addUser(userEntity)
-	// 			.then((id: string) => {
-	// 				userId = id;
-	// 				// console.log('\nPlayer Entity POST/beforeAll/POST user: ' + userId + '\n');
-	//
-	// 				// Create a tournament
-	// 				tournamentDao.add(tournamentEntity)
-	// 					.then((id: string) => {
-	// 						tournamentId = id;
-	// 						// console.log('\nPlayer Entity POST/beforeAll/POST tournament: ' + tournamentId + '\n');
-	//
-	// 						// Create a game
-	// 						request(app)
-	// 							.post("/player")
-	// 							.send(playerEntity)
-	// 							.set('Accept', 'application/json')
-	// 							.expect('Content-Type', /json/)
-	// 							.expect(201)
-	// 							.then((response: any) => {
-	// 								console.log('\nPlayer Entity PATCH /player:id Entity/POST /player: ' + response.body.id + '\n');
-	// 								expect(response.body.id).toBeTruthy()
-	// 								playerId = response.body.id;
-	// 								done();
-	// 							})
-	// 							.catch((err: any) => {
-	// 								console.log('\nPlayer Entity PATCH /player:id POST /player - valid: ' + err + '\n');
-	// 								done(err)
-	// 							});
-	// 						done();
-	// 					})
-	// 					.catch((err: any) => {
-	// 						// console.log('\nPlayer Entity POST/beforeAll/POST player: ' + err + '\n');
-	// 						done(err)
-	// 					});
-	//
-	// 			})
-	// 			.catch((err: any) => {
-	// 				// console.log('\nPlayer Entity POST/beforeAll/POST user: ' + err + '\n');
-	// 				done(err)
-	// 			});
-	// 	})
-	// 	it('patches all patchable attributes', async done => {
-	// 		let entityPatch: any = {
-	// 			hadByeOrForfeit: true,      // True after player had a bye or was awarded a forfeit
-	// 			byeNextRound: true,         // True if player is scheduled for next round bye
-	// 			playedAgainst: [""],  // Players (not users)
-	// 			playedColor: [-1],    // -1=Black, 1=White
-	// 			results: [1]        // -1 = loss, 0.5=tie, 1=win
-	// 		}
-	// 		console.log('\npatches all patchable attributes: ' + JSON.stringify(entityPatch) + '\n');
-	// 		let response = await utils.patchEntity(request(app), "/player" + '/' + playerId, entityPatch);
-	// 		expect(response.body.hadByeOrForfeit).toEqual(entityPatch.hadByeOrForfeit);
-	// 	});
-	// });
 })
