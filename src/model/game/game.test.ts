377a06cfe9463670874e538aff6c6490e06a7f02
Rodrigo Silveira <r3850365@gmail.com>
2021-02-16 20:34:18 -0800

    game  -  Implemented unit tests for PATCH /game/<id>

--- src/model/game/game.test.ts
+++ src/model/game/game.test.ts
@@ -1,3 +1,5 @@
+import {GAME_STATES} from "./game.constants";
+
 const request = require('supertest');
 
 import app from './../../index';
@@ -109,7 +111,7 @@ describe('Game Entity', () => {
 			done();
 		});
 	});
-	// Appended  the `-`
+	// Appended  the `-`to force JEST to run post prior to running this one
 	describe('GET-', () => {
 		it('/game/<id>', async done => {
 			await request(app)
@@ -121,10 +123,80 @@ describe('Game Entity', () => {
 				.then((response: any) => {
 					console.log('\nGET<id> a player: ' + response.body.id + '\n');
 					expect(response.body.tournament).toEqual(gameEntity.tournament);
+					expect(response.body.whitePiecesPlayer).toEqual(gameEntity.whitePiecesPlayer);
+					expect(response.body.blackPiecesPlayer).toEqual(gameEntity.blackPiecesPlayer);
+					expect(response.body.state).toEqual(GAME_STATES.SCHEDULED);
+					let [month, day, year]    = new Date().toLocaleDateString("en-US").split("/")
+					let testDate = new Date(response.body.date)
+					expect(testDate.getMonth()).toEqual((Number(month)-1));
+					expect(testDate.getDate()).toEqual(Number(day));
+					expect(testDate.getFullYear()).toEqual(Number(year));
 					done();
 				})
 				.catch((err: any) => {
 					console.log('\nPOST /player - invalid: ' + err + '\n');
+					done(err)
+				});
+			// console.log('\nPOSTed a player 1: ' + playerId + '\n');
+			done();
+		});
+	});
+	describe('PATCH ', () => {
+		it('/game/<id> all patchable attributes', async done => {
+			gamePatch = {
+				"state": GAME_STATES.UNDERWAY,
+				"date": "4/2/2021"
+			}
+			await request(app)
+				.patch("/game/" + gameId)
+				.send(gamePatch)
+				.set('Accept', 'application/json')
+				.expect('Content-Type', /json/)
+				.expect(200)
+				.then((response: any) => {
+					console.log('\nGET<id> a player: ' + response.body.id + '\n');
+					expect(response.body.tournament).toEqual(gameEntity.tournament);
+					expect(response.body.whitePiecesPlayer).toEqual(gameEntity.whitePiecesPlayer);
+					expect(response.body.blackPiecesPlayer).toEqual(gameEntity.blackPiecesPlayer);
+					expect(response.body.state).toEqual(GAME_STATES.UNDERWAY);
+					let testDate = new Date(response.body.date)
+					expect(testDate.getMonth()).toEqual(4 - 1);
+					expect(testDate.getDate()).toEqual(Number(2));
+					expect(testDate.getFullYear()).toEqual(Number(2021));
+					done();
+				})
+				.catch((err: any) => {
+					console.log('\nPATCH /player - invalid: ' + err + '\n');
+					done(err)
+				});
+			// console.log('\nPOSTed a player 1: ' + playerId + '\n');
+			done();
+		});
+		it('/game/<id> result', async done => {
+			gamePatch = {
+				"result": "1-0"
+			}
+			await request(app)
+				.patch("/game/" + gameId)
+				.send(gamePatch)
+				.set('Accept', 'application/json')
+				.expect('Content-Type', /json/)
+				.expect(200)
+				.then((response: any) => {
+					console.log('\nGET<id> a player: ' + response.body.id + '\n');
+					expect(response.body.tournament).toEqual(gameEntity.tournament);
+					expect(response.body.whitePiecesPlayer).toEqual(gameEntity.whitePiecesPlayer);
+					expect(response.body.blackPiecesPlayer).toEqual(gameEntity.blackPiecesPlayer);
+					let testDate = new Date(response.body.date)
+					expect(testDate.getMonth()).toEqual(4 - 1);
+					expect(testDate.getDate()).toEqual(Number(2));
+					expect(testDate.getFullYear()).toEqual(Number(2021));
+					expect(response.body.result).toEqual("1-0");
+					expect(response.body.state).toEqual(GAME_STATES.COMPLETE);
+					done();
+				})
+				.catch((err: any) => {
+					console.log('\nPATCH /player - invalid: ' + err + '\n');
 					done(err)
 				});
 			// console.log('\nPOSTed a player 1: ' + playerId + '\n');
