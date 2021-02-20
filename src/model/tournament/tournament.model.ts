2bc94746c17a516ddce4f31ccd5677d545e71491
Rodrigo Silveira <r3850365@gmail.com>
2021-02-20 13:35:02 -0800

    round  -  Refactored: - tournament.model.ts to include start and end dates - constants: tournament's constants to reflect new attributes

--- src/model/tournament/tournament.model.ts
+++ src/model/tournament/tournament.model.ts
@@ -1,3 +1,6 @@
+/**
+ *
+ */
 export interface TournamentDto {
 	id: string;				// created by the server
 	name: string;			// must be unique
@@ -12,4 +15,6 @@ export interface TournamentDto {
 	state?: string; // planned* / scheduled / closed / underway / complete
 	winPoints?: number; // points awarded for a win, 1 is default
 	tiePoints?: number; // points awarded for a tie, 0.5 is default
+	started?: number; // milliseconds since epoch to date (date when transitioned to underway)
+	ended?: number; // milliseconds since epoch to date (date when transitioned to complete)
 }
